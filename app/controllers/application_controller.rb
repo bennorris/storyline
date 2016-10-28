63037bf142ec51e1e48c97d5c832697cc3750d4b
github username <github email address>
2016-10-28 14:02:30 -0400

    switched navbar to right side

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :random_story
+
+  def random_story
+    Story.all.sample
+  end
+  
 end
