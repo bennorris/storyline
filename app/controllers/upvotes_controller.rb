f7be32a7feefa49d7390642b00319a865c4329f8
github username <github email address>
2016-10-29 11:08:41 -0400

    made sure user can only upvote sentence one time

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -2,26 +2,27 @@ class UpvotesController < ApplicationController
 
   def create
     @story = Story.find_by_id(params[:story_id])
+    @upvote = Upvote.new
 
     if params[:story_id] && !params[:sentence_id] #checking if its a story upvote or sentence upvote
-      if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == params[:story_id]}.empty? #making sure user hasnt already voted
+      if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == params[:story_id].to_i }.empty? #making sure user hasnt already voted
         @upvote = Upvote.new(user_id: current_user.id, upvotable_id: params[:story_id], upvotable_type: "Story")
+        @upvote.save
+        redirect_to story_path(@story), :flash => { :success => "Thanks for voting!" }
       else
         redirect_to story_path(@story), :flash => { :error => "Sorry, there was a problem with your vote." }
       end
     elsif params[:story_id] && params[:sentence_id]
-      if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == params[:story_id]}.empty?
+      if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == params[:story_id].to_i }.empty?
         @upvote = Upvote.new(user_id: current_user.id, upvotable_id: params[:sentence_id], upvotable_type: "Sentence")
+        @upvote.save
+        redirect_to story_path(@story), :flash => { :success => "Thanks for voting!" }
       else
         redirect_to story_path(@story), :flash => { :error => "Sorry, there was a problem with your vote." }
       end
     end
 
-    if @upvote.save
-      redirect_to story_path(@story), :flash => { :success => "Thanks for voting!" }
-    else
-      redirect_to story_path(@story), :flash => { :error => "Sorry, there was a problem with your vote." }
-    end
+
   end
 
 
