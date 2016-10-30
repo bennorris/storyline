729203e668300a548caf0bd224be3f45ecc68675
github username <github email address>
2016-10-30 15:59:08 -0400

    fixed issue with user upvoting multiple sentences in one story

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,7 +3,7 @@ class UpvotesController < ApplicationController
   def create
     @story = Story.find_by_id(params[:story_id])
     @upvote = Upvote.new
-
+    
     if params[:story_id] && !params[:sentence_id] #checking if its a story upvote or sentence upvote
       if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == params[:story_id].to_i }.empty? #making sure user hasnt already voted
         @upvote = Upvote.new(user_id: current_user.id, upvotable_id: params[:story_id], upvotable_type: "Story")
@@ -13,7 +13,7 @@ class UpvotesController < ApplicationController
         redirect_to story_path(@story), :flash => { :error => "Sorry, there was a problem with your vote." }
       end
     elsif params[:story_id] && params[:sentence_id]
-      if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == params[:story_id].to_i }.empty?
+      if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == params[:sentence_id].to_i }.empty?
         @upvote = Upvote.new(user_id: current_user.id, upvotable_id: params[:sentence_id], upvotable_type: "Sentence")
         @upvote.save
         redirect_to story_path(@story), :flash => { :success => "Thanks for voting!" }
