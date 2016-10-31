6a5f0ee2251006dfe35664391f0b856413a79d42
github username <github email address>
2016-10-31 10:37:51 -0400

    genre create now adds first sentence to story.full_story

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -7,7 +7,8 @@ class GenresController < ApplicationController
   def create
     @genre = Genre.find_or_create_by(name: params[:genre][:name])
     @story = @genre.stories.build(beginning: params[:story][:beginning], user_id: current_user.id)
-
+    @story.full_story = @story.beginning
+    
     if @genre.save && @story.save
       redirect_to user_path(current_user), :flash => { :success => "Success! You can find your story below, in the 'Your Stories' section." }
     else
