14b04dee107f41064de447c1ab6b99deec295212
github username <github email address>
2016-11-10 17:32:41 -0500

    refactored story show page to include partials and helper methods

--- app/helpers/story_helper.rb
+++ app/helpers/story_helper.rb
@@ -1,2 +1,19 @@
 module StoryHelper
+
+  def any_flash_messages?
+    if flash[:success].present?
+      flash[:success]
+    elsif flash[:error].present?
+      flash[:error]
+    end
+  end
+
+  def has_upvotes?(story)
+    if story.unique_upvotes.empty?
+      return "None, yet"
+    else
+      story.unique_upvotes.to_sentence
+    end
+  end
+
 end
