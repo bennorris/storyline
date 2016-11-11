1a8109b3e9b91f609e5a388ccb06d2cede42be04
github username <github email address>
2016-11-10 21:21:46 -0500

    refactored upvote create action to make it more DRY

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -4,25 +4,26 @@ class UpvotesController < ApplicationController
     @story = Story.find_by_id(params[:story_id])
     @upvote = Upvote.new
 
-    if params[:story_id] && !params[:sentence_id] #story upvote
+    if params[:story_id] && !params[:sentence_id]
+      user_upvote(params[:story_id], "Story")
+    elsif params[:story_id] && params[:sentence_id] 
+      user_upvote(params[:sentence_id], "Sentence")
+    end
 
-      if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == params[:story_id].to_i && vote.upvotable_type == "Story" }.empty? #making sure user hasn't already voted
-        @upvote = Upvote.new(user_id: current_user.id, upvotable_id: params[:story_id], upvotable_type: "Story")
-        @upvote.save
-        redirect_to story_path(@story), :flash => { :success => "Thanks for voting!" }
-      else
-        redirect_to story_path(@story), :flash => { :error => "Sorry, there was a problem with your vote." }
-      end
-    elsif params[:story_id] && params[:sentence_id] #sentence upvote
+  end
 
-      if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == params[:sentence_id].to_i && vote.upvotable_type == "Sentence" }.empty? #make sure user hasn't voted
-        @upvote = Upvote.new(user_id: current_user.id, upvotable_id: params[:sentence_id], upvotable_type: "Sentence")
-        @upvote.save
-        redirect_to story_path(@story), :flash => { :success => "Thanks for voting!" }
-      else
-        redirect_to story_path(@story), :flash => { :error => "Sorry, there was a problem with your vote." }
-      end
+private
+
+  def user_upvote(which_id, type)
+    if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == which_id.to_i && vote.upvotable_type == "#{type}" }.empty? #making sure user hasn't already voted
+      @upvote = Upvote.new(user_id: current_user.id, upvotable_id: which_id, upvotable_type: "#{type}")
+      @upvote.save
+      redirect_to story_path(@story), :flash => { :success => "Thanks for voting!" }
+    else
+      redirect_to story_path(@story), :flash => { :error => "Sorry, there was a problem with your vote." }
     end
   end
+
+
 
 end
