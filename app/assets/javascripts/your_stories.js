20f7467a18403542a90b6a21b9534fd44ab72eab
github username <github email address>
2016-11-29 09:50:37 -0500

    updated the next story button on user show page. the details link wasnt working before because it was a static value. now it grabs story ids from json dynamically.

--- app/assets/javascripts/your_stories.js
+++ app/assets/javascripts/your_stories.js
@@ -1,28 +1,29 @@
 var allStories = [];
+var storyIds = [];
 var storyCount = 0;
 
 function getStories() {
   $.get("/stories.json", function(data) {
     for (var i=0; i<data.length; i++) {
       allStories.push([data[i].full_story]);
+      storyIds.push([data[i].id])
       }
     })
     allStories = allStories.reverse().filter(function (e, i, arr) {
-    return allStories.indexOf(e, i+1) === -1;
-}).reverse();
+      return allStories.indexOf(e, i+1) === -1;}).reverse();
 
   }
 
 function updateCount() {
   storyCount += 1;
-
    console.log(storyCount);
 }
 
 function nextStory() {
   $('#user-story-full').text(allStories[storyCount]);
+  $('#next-user-story').attr('href', '/stories/' + storyIds[storyCount]);
   updateCount();
-}
+} //reset storyCount to 0 once it hits the length of the array
 
 function thisWasClicked() {
 $(document).on('click', '#your-stories-button', function(e) {
