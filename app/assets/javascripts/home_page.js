960005bf2e2d60b61305d764915341e3e59da8bb
github username <github email address>
2016-11-30 21:00:05 -0500

    moved js from show page to home_page.js

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -1,6 +1,9 @@
+$(function() {
+
+// SCROLLING THROUGH ALL STORIES
 $(document).on('click', '#next-button', function() {
     $('#story-preview').empty();
-    
+
     var rand = storiesArray[Math.floor(Math.random() * storiesArray.length)];
     if (rand[2] == false) {
       $('#story-preview').append(
@@ -12,4 +15,34 @@ $(document).on('click', '#next-button', function() {
         '<p style="color: #EF9A9A">You were the most recent to contribute. You can add a new sentence after someone else has.</p><br>' +
         '<a href="/stories/'+rand[1]+'">see contributors & more</a>');
     }
-})
+});
+
+// DROP DOWN LIST OF ALL USER STORIES ON CLICK
+
+var FullStory = function(story, id, upvoteSize) {
+  this.story = story;
+  this.id = id;
+  this.upvoteSize = upvoteSize;
+}
+
+function formatter(arg) {
+  return arg.story + '<br>' + "<a href='/stories/" + arg.id + "'>details</a>" + "&nbsp;&nbsp;|&nbsp;&nbsp<a href='/stories/" + arg.id + "' data-method='delete' data-confirm='Are you sure you want to delete this?'>delete</a>&nbsp&nbsp<p style='font-size:20px;color:#0D47A1'>upvotes: " + arg.upvoteSize + '</p><br><br>'
+}
+
+  var open = false;
+  $('#view-all-button').on('click', function() {
+    if (open == false) {
+    $.get('/users/' + user + '/stories.json', function(data) {
+      for (var i=0;i<data.length;i++) {
+        var story = new FullStory(data[i].full_story, data[i].id, data[i].upvotes.length)
+        $('#story-list').append(formatter(story));
+
+    };
+  });
+    open = true;
+  } else if (open == true) {
+      $('#story-list').html('');
+      open = false;
+    }
+  });
+});
