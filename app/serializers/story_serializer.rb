cf6cc2dff1b3d0d748d8ebd92fd7054d6001e725
github username <github email address>
2016-12-07 08:59:02 -0500

    added belongs_to relationship on upvotes and added beginning value on stories

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,4 +1,4 @@
 class StorySerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :full_story
+  attributes :id, :user_id, :full_story, :beginning
   has_many :upvotes
 end
--- app/serializers/upvote_serializer.rb
+++ app/serializers/upvote_serializer.rb
@@ -1,4 +1,4 @@
 class UpvoteSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :upvotable_id, :upvotable_type
-  
+  belongs_to :story
 end
