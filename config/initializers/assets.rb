500945938f41e9c001db2159fd3f529c04105bea
github username <github email address>
2016-12-06 21:24:42 -0500

    moved js from view to its own file for new story submission

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( home_page.js )
 Rails.application.config.assets.precompile += %w( your_stories.js )
 Rails.application.config.assets.precompile += %w( your_sentences.js )
-
+Rails.application.config.assets.precompile += %w( new_story.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
