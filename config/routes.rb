7a55dd2fbe1c7f2b405929ca48f6f2e4f107c541
github username <github email address>
2016-12-07 22:19:09 -0500

    added routes for json

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", :registrations => 'registrations' }
 
   root 'users#show'
-  resources :users , only: [:create, :new, :edit, :update, :destroy, :show]
-  get '/users/:id/stories', to: 'users#index', as: "all_user_stories"
+  resources :users , only: [:create, :new, :edit, :update, :destroy, :show] do
+    resources :stories, only: [:index, :show]
+    resources :sentences, only: [:index]
+  end
+
   resources :stories, only: [:create, :new, :show, :destroy, :index] do
     resources :upvotes, only: [:create, :show]
     resources :sentences do
@@ -15,4 +18,5 @@ Rails.application.routes.draw do
   get '/user/:id/stats', to: 'users#stats', as: "user_stats"
   post '/users', to: 'users#create'
   get '/sentences', to: 'sentences#index'
+  get '/get_id', to: 'users#get_id'
 end
