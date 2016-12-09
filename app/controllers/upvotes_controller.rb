83fd5870e231ff071d1dcf8146c27369c32499d1
github username <github email address>
2016-12-09 11:53:44 -0500

    made control thinner - took out unneccesary stuff post-refactor

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -22,15 +22,16 @@ class UpvotesController < ApplicationController
 
 
 
+
+
 private
 
   def user_upvote(which_id, type)
     if Upvote.all.select {|vote| vote.user_id == current_user.id && vote.upvotable_id == which_id.to_i && vote.upvotable_type == "#{type}" }.empty? #making sure user hasn't already voted
       @upvote = Upvote.new(user_id: current_user.id, upvotable_id: which_id, upvotable_type: "#{type}")
       @upvote.save
-      redirect_to story_path(@story), :flash => { :success => "Thanks for voting!" }
     else
-      redirect_to story_path(@story), :flash => { :error => "Sorry, there was a problem with your vote." }
+      return false;
     end
   end
 
